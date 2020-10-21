8135a1348f24d71c53bf4c32f828a91079fa88e5
Suslikov Daniil <danyvs@mail.ru>
2020-10-21 17:48:50 +0300

    lab2

--- lab1/commands.sql
+++ lab1/commands.sql
@@ -1,13 +1,13 @@
 create table if not exists studios(
     studio_id int primary key,
-    studio_name varchar(40),
+    studio_name varchar(40) not null,
     date_of_creation date
 );
 copy studios from 'D:\BD\studios.csv' delimiter ',' csv; 
 
 create table if not exists directors(
     director_id int primary key,
-    director_name varchar(40),
+    director_name varchar(40) not null,
     age int check(age > 23 and age < 71),
     sex varchar (6),
     films_amount int check(films_amount > 0 and films_amount < 10)
@@ -18,7 +18,7 @@ create table if not exists films(
     film_id int primary key,
     studio_id int references studios(studio_id),
     director_id int references directors(director_id),
-    film_name varchar(40),
+    film_name varchar(40) not null,
     release_date date,
     budget int check(budget >= 20000 and budget <= 250000000),
     fees int check(fees >= 20000 and fees <= 1000000000)
@@ -28,7 +28,7 @@ copy films from 'D:\BD\films.csv' delimiter ',' csv;
 create table if not exists actors(
     actor_id int primary key,
     film_id int references films(film_id),
-    actor_name varchar(40),
+    actor_name varchar(40) not null,
     age int check(age > 17 and age < 71),
     sex varchar(6),
     nationality varchar(40),
